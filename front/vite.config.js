0e01ad620d1d58fbd966945c0d1c47cfa1834905
Leman-Mustafazadeh <sonaja@code.edu.az>
2024-10-01 18:39:02 +0400

    .

--- front/vite.config.js
+++ front/vite.config.js
@@ -4,9 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['build.rollupOptions.external'], // Replace with your module name
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     external: ['build.rollupOptions.external'], // Replace with your module name
+  //   },
+  // },
+   build:{
+    outDir: "build"
+  }
 });
